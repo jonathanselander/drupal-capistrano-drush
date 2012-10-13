72ffc2bb5e2c376faf75d276f2f66e936569fb81
Jonathan Selander <jonathan@mondaycreative.se>
2012-10-13 21:19:22 +0200

    gitignore

--- profiles/site/themes/mattias/templates/block.tpl.php
+++
@@ -1,8 +0,0 @@
-<section class="<?php print $block_html_id; ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($block->subject): ?>
-    <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
-  <?php endif;?>
-  <?php print render($title_suffix); ?>
-  <?php print $content ?>
-</section>
--- profiles/site/themes/mattias/templates/user-profile.tpl.php
+++
@@ -1,6 +0,0 @@
-<article class="profile">
-  <?php hide($user_profile['summary']); ?>
-  <?php print render($user_profile); ?>
-  <h6>You look great today, <?php print $user_profile['name']; ?>!</h6>
-  <p>Now, get some stuff done will you.</p>
-</article>
